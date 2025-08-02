9ef6217203a8976dfb2177b09c37ae37c0484156
Bogdanelo <bodyalazebnik2006@gmail.com>
2025-08-02 23:03:19 +0300

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -132,19 +132,6 @@ window.addEventListener('scroll', function () {
 
 
 
-// вставка зображень в mobile-social-list"
-const githubLogo = document.getElementById('github-logo-mobile-menu');
-const dribbbleLogo = document.getElementById('dribble-logo-mobile-menu');
-const figmaLogo = document.getElementById('figma-logo-mobile-menu');
-
-const svgFiles = [
-  '/assets/img/logo/github.svg',
-  '/assets/img/logo/dribble.svg',
-  '/assets/img/logo/figma.svg'
-];
-
-const logos = [githubLogo, dribbbleLogo, figmaLogo];
-
 logos.forEach((logo, index) => {
   const svgFile = svgFiles[index];
   fetch(svgFile)
